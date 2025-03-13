9ee59c1b8ff75cb22e52d3e816e9f60e1d3f511c
dev-araujo <theriancirculo@protonmail.com>
2025-03-13 17:42:04 -0300

    feat: change goho token for more simple

--- smart-contracts/src/GoHorseToken.sol
+++ smart-contracts/src/GoHorseToken.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-import "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-error InvalidInitialSupply(); /// @notice Erro lançado quando a quantidade inicial de tokens excede o limite máximo.
-error OnlyOwner(); /// @notice Erro lançado quando uma função restrita ao proprietário é chamada por outro endereço.
-error MaxSupplyExceeded(); /// @notice Erro lançado quando a mintagem ultrapassa o suprimento máximo.
+error ExceedsMaxSupply(); // Erro lançado quando a mintagem excede o suprimento máximo.
+error InvalidInitialSupply(); // Erro lançado quando o suprimento inicial excede o limite máximo.
 
 /**
  * @title GoHorse Token (GOHO)
@@ -13,83 +13,45 @@ error MaxSupplyExceeded(); /// @notice Erro lançado quando a mintagem ultrapass
  * @notice Implementação de um token ERC20 para o projeto GoHorse.
  * @dev Utiliza a biblioteca OpenZeppelin para implementação do padrão ERC20.
  */
-contract GoHorse is ERC20 {
-    uint256 private constant TOKEN_MULTIPLIER = 10 ** 18;
-    address private s_owner;
+
+contract GoHorse is ERC20, Ownable {
+    uint256 public constant MAX_SUPPLY = 5000 * 10 ** 18;
     string private s_metadataUrl;
-    mapping(address => bool) private minters;
-
-    uint256 public constant MAX_SUPPLY = 5000 * TOKEN_MULTIPLIER;
-
-    /// @notice Evento emitido quando o suprimento inicial é mintado.
-    event InitialSupplyMinted(address indexed to, uint256 amount);
-    event MinterUpdated(address indexed minter, bool status);
+    uint256 private s_totalMinted;
 
     /**
      * @notice Construtor do contrato do token.
-     * @param initialSupply Quantidade inicial de tokens a serem cunhados.
+     * @param initialSupply Quantidade inicial de tokens a serem mintados.
      * @param metadataUrl URL de metadados do token.
      */
     constructor(
         uint256 initialSupply,
         string memory metadataUrl
-    ) ERC20("Go Horse", "GOHO") {
-        s_owner = msg.sender;
-        s_metadataUrl = metadataUrl;
-        minters[msg.sender] = true;
-        _mintInitialSupply(s_owner, initialSupply * TOKEN_MULTIPLIER);
-    }
-
-    modifier onlyOwner() {
-        if (msg.sender != s_owner) {
-            revert OnlyOwner();
-        }
-        _;
-    }
-
-    modifier onlyMinter() {
-        require(minters[msg.sender], "Not authorized to mint");
-        _;
-    }
-
-    /**
-     * @notice Define um novo minter para o contrato.
-     * @param minter O endereço que poderá mintar tokens.
-     * @param isAllowed Booleano indicando se o endereço pode ou não mintar.
-     */
-    function setMinter(address minter, bool isAllowed) external onlyOwner {
-        minters[minter] = isAllowed;
-        emit MinterUpdated(minter, isAllowed);
-    }
-
-    /**
-     * @notice Mintagem inicial de tokens.
-     * @param to O endereço que receberá os tokens.
-     * @param amount A quantidade de tokens a serem mintados.
-     */
-    function _mintInitialSupply(address to, uint256 amount) internal {
-        if (amount > MAX_SUPPLY) {
+    ) ERC20("Go Horse", "GOHO") Ownable(msg.sender) {
+        if (initialSupply > MAX_SUPPLY) {
             revert InvalidInitialSupply();
         }
-        _mint(to, amount);
-        emit InitialSupplyMinted(to, amount);
+        s_metadataUrl = metadataUrl;
+        _mint(msg.sender, initialSupply * 10 ** decimals());
+        s_totalMinted = initialSupply * 10 ** decimals();
     }
 
     /**
      * @notice Mintagem de novos tokens.
-     * @dev Apenas minters podem mintar novos tokens.
-     * @param to O endereço que receberá os tokens.
-     * @param amount A quantidade de tokens a serem mintados.
+     * @dev Apenas o proprietário pode mintar novos tokens.
+     * @param to Endereço que receberá os tokens.
+     * @param amount Quantidade de tokens a serem mintados.
      */
-    function mint(address to, uint256 amount) external onlyMinter {
-        if (totalSupply() + amount > MAX_SUPPLY) {
-            revert MaxSupplyExceeded();
+    function mint(address to, uint256 amount) external onlyOwner {
+        if (s_totalMinted + amount > MAX_SUPPLY) {
+            revert ExceedsMaxSupply();
         }
         _mint(to, amount);
+        s_totalMinted += amount;
     }
 
     /**
-     * @notice Obtém a URL de metadados do token.
+     * @notice Retorna a URL de metadados do token.
      * @return A URL contendo informações sobre o token (imagem, descrição, etc.).
      */
     function getMetadataUrl() external view returns (string memory) {
@@ -97,10 +59,18 @@ contract GoHorse is ERC20 {
     }
 
     /**
-     * @notice Retorna o endereço do proprietário do contrato.
-     * @return O endereço do proprietário.
+     * @notice Retorna o total de tokens mintados.
+     * @return O total de tokens mintados.
      */
-    function owner() external view returns (address) {
-        return s_owner;
+    function getTotalMinted() external view returns (uint256) {
+        return s_totalMinted;
+    }
+
+    /**
+     * @notice Retorna o suprimento máximo de tokens.
+     * @return O suprimento máximo de tokens.
+     */
+    function getMaxSupply() external pure returns (uint256) {
+        return MAX_SUPPLY;
     }
 }
