29e5bb14f742a9c2091ac81f0cc46d8ce5f110d8
dev-araujo <theriancirculo@protonmail.com>
2025-04-08 16:27:02 -0300

    refactor: lazy loading

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,25 @@
-import { AboutComponent } from './features/about/about.component';
-import { DocumentationComponent } from './features/documentation/documentation.component';
-import { MintComponent } from './features/mint/mint.component';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: 'token', component: MintComponent, title: 'Token GoHorse' },
-  { path: 'sobre', component: AboutComponent, title: 'Sobre GoHorse' },
-  { path: 'docs', component: DocumentationComponent, title: 'Docs GoHorse' },
+  {
+    path: 'token',
+    loadComponent: () =>
+      import('./features/mint/mint.component').then((m) => m.MintComponent),
+    title: 'Token GoHorse',
+  },
+  {
+    path: 'sobre',
+    loadComponent: () =>
+      import('./features/about/about.component').then((a) => a.AboutComponent),
+    title: 'Sobre GoHorse',
+  },
+  {
+    path: 'docs',
+    loadComponent: () =>
+      import('./features/documentation/documentation.component').then(
+        (d) => d.DocumentationComponent
+      ),
+    title: 'Docs GoHorse',
+  },
   { path: '**', redirectTo: '/token', pathMatch: 'full' },
 ];
